c6cdad94deb20f782ba898739cea204c7aa82ed1
Jürgen Eckel <eckelj@users.noreply.github.com>
2022-12-12 21:02:13 +0100

    Better cli docs (#5)
    * improved the typer cli --help output
    * blackified
    * added encryption support improved data consistency
    * fixed 0x21e8 compatiblity issue
    
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- rddl_client/attest.py
+++ rddl_client/attest.py
@@ -1,26 +1,37 @@
 import urllib3
 import json
+import ast
 from .config import HW_03_SERVICE
 from .config import TASMOTA_SERVICE
 
 
-def store(data: dict):
+def store(data: dict, encrypt: bool = False):
     http = urllib3.PoolManager()
-    cid_resp = http.request("POST", HW_03_SERVICE + "/data", headers={"Content-Type": "application/json"}, body=data)
-    return cid_resp.data.decode()
+    dict_string = json.dumps(data)
+    cid_resp = http.request(
+        "POST",
+        HW_03_SERVICE + "/data?encrypt=" + str(encrypt),
+        headers={"Content-Type": "application/json"},
+        body=dict_string,
+    )
+    cid_str = cid_resp.data.decode()
+    cid_dict = ast.literal_eval(cid_str)
+    return cid_dict
 
 
 def get_energy_data():
     http = urllib3.PoolManager()
     consumption = http.request("GET", TASMOTA_SERVICE + "/cm?cmnd=Status%208")
-    return consumption.data.decode()
+    data = consumption.data.decode()
+    cid_dict = ast.literal_eval(data)
+    return cid_dict
 
 
 def get_fake_energy_data():
     import datetime
 
     x = str(datetime.datetime.now())
-    dd = {
+    data_dict = {
         "StatusSNS": {
             "Time": x,
             "ENERGY": {
@@ -39,12 +50,12 @@ def get_fake_energy_data():
             "TempUnit": "C",
         }
     }
-    return json.dumps(dd)
+    return data_dict
 
 
 def attest_cid(cid: str):
     http = urllib3.PoolManager()
-    tx_id = http.request("POST", HW_03_SERVICE + "/cid?cid=" + cid)
+    tx_id = http.request("POST", HW_03_SERVICE + "/assets?cid=" + cid)
     return tx_id.data.decode()
 
 
@@ -62,11 +73,7 @@ def get_0x21e8_config():
 
 
 def get_and_attest_energy():
-    data = get_fake_energy_data()
-    cid = store(data)
-    print(f"CID: {cid}")
-    tx_id = attest_cid(cid)
+    data = get_energy_data()
+    cid_dict = store(data, False)
+    tx_id = attest_cid(cid_dict["cid"])
     print(f"TX ID: {tx_id}")
-    cfg = get_0x21e8_config()
-    # print( cfg)
-    # print(f"{ cfg['planetmint'] }/api/v1/transactions/{json.loads(tx_id)['NFT token']}")
